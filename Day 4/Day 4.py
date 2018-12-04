174a9dbca831607451800eb1055ac0db000234b9
blazejdolicki <blazej.dolicki@wp.pl>
2018-12-04 08:52:39 +0100

    Day 4

---
+++ Day 4/Day 4.py
@@ -0,0 +1,73 @@
+import re
+import pandas as pd
+guard_logs = open("input.txt").read().split("\n")
+guard_dict = {}
+for log in guard_logs[:-1]:
+    date = str(re.search("\[(.*)\]", log).group(1))
+    event = str(re.search("\] (.*)", log).group(1))
+    guard_dict[date] = event
+guard_logs = pd.Series(guard_dict).sort_index()
+print(guard_logs.shape)
+guard_naps={}
+print(guard_logs.index)
+print()
+counter=0
+print(guard_logs.tail())
+for i in guard_logs.index:
+    log = guard_logs[i]
+
+    if "Guard" in log:
+        guard_id = int(re.search("#(.*) b",log).group(1))
+        if not guard_id in guard_naps:
+            guard_naps[guard_id]={}
+            guard_naps[guard_id]["Total minutes"] = 0
+        print(log)
+        print(guard_id)
+    if "falls asleep"==log:
+        print(counter)
+        start_nap = int(re.search(":(.*)",guard_logs.index[counter]).group(1))
+    if "wakes up"==log:
+        stop_nap = int(re.search(":(.*)",guard_logs.index[counter]).group(1))
+        guard_naps[guard_id]['Total minutes'] += stop_nap-start_nap
+        for j in range(start_nap,stop_nap):
+            if j in guard_naps[guard_id]:
+                guard_naps[guard_id][j] += 1
+            else:
+                guard_naps[guard_id][j] = 1
+    counter+=1
+print(guard_naps)
+
+most_minutes_slept = 0
+most_sleepy_guard = 0
+for guard in guard_naps:
+    if guard_naps[guard]['Total minutes'] > most_minutes_slept:
+        most_minutes_slept = guard_naps[guard]['Total minutes']
+        most_sleepy_guard = guard
+most_sleepy_minute = 0
+most_sleep = 0
+for minute in guard_naps[most_sleepy_guard]:
+    if minute!='Total minutes':
+        if guard_naps[most_sleepy_guard][minute]>most_sleep:
+            most_sleepy_minute = minute
+            most_sleep = guard_naps[most_sleepy_guard][minute]
+print("Answer",most_sleepy_guard*most_sleepy_minute)
+
+# Part 2
+most_frequently_asleep_minute = 0
+highest_sleep_frequency = 0
+most_frequently_asleep_guard = 0
+for guard in guard_naps:
+    for minute in guard_naps[guard]:
+        if minute != 'Total minutes':
+            if guard_naps[guard][minute]>highest_sleep_frequency:
+                most_frequently_asleep_minute = minute
+                highest_sleep_frequency = guard_naps[guard][minute]
+                most_frequently_asleep_guard = guard
+print("Answer",most_frequently_asleep_guard*most_frequently_asleep_minute)
+
+# for each entry
+# get the number of guard
+# get start of his nap
+# get end
+# for this range, add minutes during which he was asleep as keys (or increment key if it's already there)
+#
